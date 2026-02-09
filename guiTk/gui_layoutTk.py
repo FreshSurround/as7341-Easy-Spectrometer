7827384d0cd29315f486a305ff1db5e84b788ced
Laurie <54045431+FreshSurround@users.noreply.github.com>
2026-02-09 17:25:33 +0000

    gui_layoutTk.py
    Added a the space necessary for the histogram. Also reorganized the layout in a more "minimalist" way, with the buttons and sliders in the right instead of laying around in the screen.

--- guiTk/gui_layoutTk.py
+++ guiTk/gui_layoutTk.py
@@ -6,41 +6,33 @@ from .gui_controlsTk import *
 class SpectrometerGUI(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-
+        self.controller = GUIControls()
         self._create_layout()
 
     def _create_layout(self):
-        # --- Frames principales ---
-        self.top_frame = ttk.Frame(self)
-        self.top_frame.pack(side=tk.TOP, fill=tk.X)
-
-        self.center_frame = ttk.Frame(self)
-        self.center_frame.pack(expand=True, fill=tk.BOTH)
-
+        
         self.right_frame = ttk.Frame(self, width=200)
         self.right_frame.pack(side=tk.RIGHT, fill=tk.Y)
 
         self.bottom_frame = ttk.Frame(self)
         self.bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        
+        self.btn_connect = ttk.Button(self.right_frame, text="Conectar")
+        self.btn_start = ttk.Button(self.right_frame, text="Iniciar")
+        self.btn_stop = ttk.Button(self.right_frame, text="Detener")
+        self.btn_disc = ttk.Button(self.right_frame, text="Desconectar")
+        
+        self.btn_connect.pack(padx=5, pady=5)
+        self.btn_start.pack(padx=5, pady=5)
+        self.btn_stop.pack(padx=5, pady=5)
+        self.btn_disc.pack(padx=5, pady=5)
+        
+        self.histogram_frame = tk.Frame(self, bg="black")
+        self.histogram_frame.pack(fill="both", expand=True)
 
-        # --- Top: controles principales ---
-        self.btn_connect = ttk.Button(self.top_frame, text="Conectar")
-        self.btn_start = ttk.Button(self.top_frame, text="Iniciar")
-        self.btn_stop = ttk.Button(self.top_frame, text="Detener")
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
 
-        self.btn_connect.pack(side=tk.LEFT, padx=5, pady=5)
-        self.btn_start.pack(side=tk.LEFT , padx=5, pady=5)
-        self.btn_stop.pack(side=tk.LEFT, padx=5, pady=5)
-
-        # --- Centro: área de gráfico ---
-        self.plot_area = ttk.Label(
-            self.center_frame,
-            text="Área de visualización del espectro",
-            anchor="center"
-        )
-        self.plot_area.pack(expand=True, fill=tk.BOTH, padx=10, pady=10)
-
-        # --- Derecha: parámetros ---
         ttk.Label(self.right_frame, text="Parámetros").pack(pady=5)
 
         self.gain_slider = ttk.Scale(self.right_frame, from_=0, to=100)
@@ -51,7 +43,6 @@ class SpectrometerGUI(tk.Frame):
         ttk.Label(self.right_frame, text="Promediado").pack()
         self.avg_slider.pack(fill=tk.X, padx=10)
 
-        # --- Bottom: estado ---
         self.status_label = ttk.Label(
             self.bottom_frame,
             text="Estado: desconectado",
